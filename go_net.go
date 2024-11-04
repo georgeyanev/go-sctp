072b727a5086f51efc30ea7e511c04de8f352246
George Yanev <george.yanef@gmail.com>
2024-11-04 17:07:19 +0200

    Add accept func from go net package

--- go_net.go
+++ go_net.go
@@ -53,6 +53,43 @@ func sysSocket(family, sotype, proto int) (int, error) {
 	return s, nil
 }
 
+// Wrapper around the accept system call that marks the returned file
+// descriptor as nonblocking and close-on-exec.
+func accept(s int) (int, unix.Sockaddr, string, error) {
+	ns, sa, err := unix.Accept4(s, syscall.SOCK_NONBLOCK|syscall.SOCK_CLOEXEC)
+	// TODO: We can remove the fallback on Linux and *BSD,
+	// as currently supported versions all support accept4
+	// with SOCK_CLOEXEC, but Solaris does not. See issue #59359.
+	switch err {
+	case nil:
+		return ns, sa, "", nil
+	default: // errors other than the ones listed
+		return -1, sa, "accept4", err
+	case syscall.ENOSYS: // syscall missing
+	case syscall.EINVAL: // some Linux use this instead of ENOSYS
+	case syscall.EACCES: // some Linux use this instead of ENOSYS
+	case syscall.EFAULT: // some Linux use this instead of ENOSYS
+	}
+
+	// See ../syscall/exec_unix.go for description of ForkLock.
+	// It is probably okay to hold the lock across syscall.Accept
+	// because we have put fd.sysfd into non-blocking mode.
+	// However, a call to the File method will put it back into
+	// blocking mode. We can't take that risk, so no use of ForkLock here.
+	ns, sa, err = unix.Accept(s)
+	if err == nil {
+		syscall.CloseOnExec(ns)
+	}
+	if err != nil {
+		return -1, nil, "accept", err
+	}
+	if err = syscall.SetNonblock(ns, true); err != nil {
+		_ = unix.Close(ns)
+		return -1, nil, "setnonblock", err
+	}
+	return ns, sa, "", nil
+}
+
 // favoriteAddrFamily returns the appropriate address family for the
 // given network, laddr, raddr and mode.
 //
