0b17a479cc6d19c1c52f62b14dca5ccff79fc9e8
George Yanev <george.yanef@gmail.com>
2024-12-05 11:30:05 +0200

    Add test from protoconn_test.go

--- sctp.go
+++ sctp.go
@@ -135,7 +135,6 @@ type RcvInfo struct {
 	AssocID int32
 }
 
-// TODO: Add test from net.protoconn_test.go (regarding specific methods of Conn, SCTPConn etc)
 // TODO: Add test from timeout_test.go
 // TODO: Add test from write_unix_test.go
 // TODO: Add test from conn_test.go
