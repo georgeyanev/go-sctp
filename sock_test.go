52e0f5cd67544d48248c6d5261141e03277385d7
George Yanev <george.yanef@gmail.com>
2024-12-09 21:05:05 +0200

    Add sx91 renamed perf-test

--- sock_test.go
+++ sock_test.go
@@ -1,3 +1,9 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the GO_LICENSE file.
+
+// This file contains test code from go's net package, tweaked for SCTP where necessary.
+
 package sctp
 
 import (
